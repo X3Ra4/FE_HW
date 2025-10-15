a9ccdb7e122d9639d4dda1d7228bffa88dc7403a
Oleg Ogienko <olgeoffoff@gmail.com>
2025-10-15 23:06:25 +0300

    hw_7-8

---
+++ HW_8/venv/script.js
@@ -0,0 +1,19 @@
+//10.1
+let company = {
+  sales: [{name: 'John', salary: 1000},{name:'alice', salary: 600}],
+  development: {
+    web: [{name: 'Peter', salary: 2000}, {name: 'Alex', salary: 1800}],
+    internals: [{name: 'Jack', salary: 1300}],
+  }
+};
+let sum = 0;
+function GetObj (department) {
+  if (Array.isArray(department)) {
+    sum += department.salary;
+  };
+  for (let i in department) {
+    GetObj(i[key]);
+  };
+};
+GetObj(company);
+console.log(sum);
