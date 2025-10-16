d599478f2356555c4ea4064d74b4d5738242fd39
Oleg Ogienko <olgeoffoff@gmail.com>
2025-10-16 19:29:51 +0300

    fix HW_8

--- HW_8/venv/script.js
+++ HW_8/venv/script.js
@@ -9,11 +9,14 @@ let company = {
 let sum = 0;
 function GetObj (department) {
   if (Array.isArray(department)) {
+    for (let person of department) {
     sum += department.salary;
-  };
-  for (let i in department) {
-    GetObj(i[key]);
-  };
-};
+  }
+  } else {
+    for (let i in department) {
+      GetObj(i[key]);
+    }
+  }
+}
 GetObj(company);
 console.log(sum);
